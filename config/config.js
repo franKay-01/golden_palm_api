d379ea48e748dd3ba438fb1dae98b7ed6fa98a16
Francis Kornu <fkay0450@gmail.com>
2025-10-29 20:25:30 +0000

    feat: using env config for db

--- config/config.js
+++ config/config.js
@@ -1,8 +1,11 @@
 const config = {
   development: {
-    username: "golden_palm_user",
-    password: null,
-    database: "golden_palm_db",
+    // username: "golden_palm_user",
+    // password: null,
+    // database: "golden_palm_db",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     host: "localhost",
     dialect: "postgres",
   },
